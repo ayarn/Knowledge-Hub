2eaab689faff0a9287aedb36bc1240343fa5bcae
Ayarn <iamayarn2001@gmail.com>
2021-10-22 09:07:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
+import axios from "axios";
 
 function App() {
+
+  const [ user, setLoginUser] = useState({})
+  const [questions, setQuestions] = useState();
+  const [name, setName] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+
+    setQuestions(data.results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App" style={{ backgroundImage: 'url("/bgimg.jpg")' }}>
+
+      <Header />
+      
+      <Switch>
+
+        <Route path="/" exact>
+          <Login setLoginUser={setLoginUser} />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+
+        <Route path='/home' exact>
+          <Home 
+            name={name}
+            setName={setName}
+            fetchQuestions={fetchQuestions}
+          />
+        </Route>
+
+        <Route path='/quiz' exact>
+          <Quiz 
+            name={name}
+            questions={questions}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
+        </Route>
+
+        <Route path='/result' exact>
+          <Result 
+            name={name} score={score}
+          />
+        </Route>
+  
+      </Switch>
+
+      </div>
+
+      <Footer />
+
+    </BrowserRouter>
+    
   );
 }
 
